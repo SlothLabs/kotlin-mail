09ca8d0e54ae47804169b14d8e5271462a7f3279
Matthew Cory <Matt.Cory@Charter.com>
2016-04-15 16:51:36 -0600

    Documentation.

--- src/main/kotlin/io/github/slothLabs/mail/imap/Folder.kt
+++ src/main/kotlin/io/github/slothLabs/mail/imap/Folder.kt
@@ -6,19 +6,46 @@ import org.funktionale.option.Option
 import javax.mail.FetchProfile
 import javax.mail.Folder
 
+/**
+ * A statically-typed `enum` to wrap the JavaMail Folder constants.
+ */
 enum class FolderModes(private val javaMailMode: Int) {
+    /**
+     * Indicates to open the folder in read-only mode.
+     */
     ReadOnly(Folder.READ_ONLY),
+
+    /**
+     * Indicates to open the folder in read/write mode.
+     */
     ReadWrite(Folder.READ_WRITE);
 
     internal fun toJavaMailMode() = javaMailMode
 }
 
+/**
+ * Wrapper class to provide additional functionality over the JavaMail `IMAPFolder`
+ * class.
+ */
 class Folder(private val javaMailFolder: IMAPFolder) {
 
+    /**
+     * The messages within this folder.
+     */
     val messages = MessageContainer(javaMailFolder)
 
     private var preFetchInfo = FetchProfile()
 
+    /**
+     * Search this folder using the [SearchBuilder] initialized within
+     * the given block, using any pre-fetch information set via either
+     * [preFetchBy] method.
+     *
+     * @param block the block to use to initialize the [SearchBuilder].
+     *
+     * @return a [List] of [Message] instances that match the search, pre-fetched
+     *         if applicable.
+     */
     fun search(block: SearchBuilder.() -> Unit): List<Message> {
         val builder = SearchBuilder()
         builder.block()
@@ -36,15 +63,32 @@ class Folder(private val javaMailFolder: IMAPFolder) {
         return messages
     }
 
+    /**
+     * Specifies a `FetchProfile` to use when fetching messages.
+     *
+     * @param fetchProfile the `FetchProfile` to fetch messages with.
+     */
     fun preFetchBy(fetchProfile: FetchProfile) {
         preFetchInfo = fetchProfile
     }
 
+    /**
+     * Allows applying multiple `FetchProfileItem`s a little easier than
+     * the standard `FetchProfile`-construction-then-add means.
+     *
+     * @param test the `FetchProfileItem`(s) to build the new prefetch information with.
+     */
     fun preFetchBy(vararg test: IMAPFolder.FetchProfileItem) {
         preFetchInfo = FetchProfile()
         test.forEach { preFetchInfo.add(it) }
     }
 
+    /**
+     * Closes the underlying JavaMail folder, expunging deleted messages or not depending
+     * on the value of `expunge`.
+     *
+     * @param expunge whether or not to expunge the underlying messages.
+     */
     fun close(expunge: Boolean) {
         javaMailFolder.close(expunge)
     }
